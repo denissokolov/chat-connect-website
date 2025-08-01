4845ed4a66d0ab1be1a430811e420c96d5ba2dc8
denissokolov <dnssokolov@gmail.com>
2025-08-01 14:50:58 +0200

    enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import clsx from 'clsx'
+import { Analytics } from '@vercel/analytics/next'
 
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
@@ -35,6 +36,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
